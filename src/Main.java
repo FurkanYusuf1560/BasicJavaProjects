263902f7354540517cee9683d6b69b8b811542db
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2025-09-28 17:09:48 +0300

    5. uygulama

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+    int s1, s2, select;
+
+        System.out.print("Lütfen bir sayı giriniz : ");
+
+        Scanner input = new Scanner(System.in);
+        s1 = input.nextInt();
+
+        System.out.print("Lütfen ikinci  sayıyı giriniz : ");
+        s2 = input.nextInt();
+
+        System.out.println("Lütfen yapmak istediğiniz işlemi seçin");
+        System.out.println("1. Toplama \n 2. Çıkarma \n 3. Çarpma \n 4.Bölme ");
+
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+            System.out.println(s1 + s2);
+            break;
+            case 2:
+                System.out.println(s1 - s2);
+                break;
+            case 3:
+                System.out.println(s1 * s2);
+                break;
+            case 4:
+                if (s2 != 0) {
+                    System.out.println(s1 / s2);
+                } else
+                    System.out.println("bölmede payda sıfır olmaz");
+
+                break;
+            default:
+                System.out.println("d");
         }
+
+
+
+
     }
 }
