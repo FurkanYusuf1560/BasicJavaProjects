4ca3919a39ca4567ca7d5a77da7a8cfffaa1ba58
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2025-10-20 11:05:51 +0300

    SayıSıralmaProgramı

---
+++ src/Metotogrenme.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Metotogrenme {
+
+    static void recursiveMethod(int num) {
+        num--;
+        if (num == 0)
+            return;
+        System.out.print(num + ",");
+        recursiveMethod(num);
+    }
+    public static void main(String[] args) {
+
+        recursiveMethod(4);
+
+    }
+
+
+
+
+
+
+
+}
