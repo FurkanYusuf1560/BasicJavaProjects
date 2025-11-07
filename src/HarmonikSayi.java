07f3dad5e74d159d2ff44b9b8843c00453d7a42c
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2025-11-07 11:44:23 +0300

    Harmonik Hesaplama

---
+++ src/HarmonikSayi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HarmonikSayi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n;
+        n = inp.nextInt();
+        double result = 0.0;
+        for (double i =1; i<=n;i++){
+             result += (1/i);
+        }
+        System.out.println(result);
+    }
+}
