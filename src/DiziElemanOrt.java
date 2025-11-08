8937a399b0c85b714279328e8aaa637501310c38
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2025-11-08 13:10:38 +0300

    dizi elemanlarını toplama

---
+++ src/DiziElemanOrt.java
@@ -0,0 +1,16 @@
+public class DiziElemanOrt {
+    public static void main(String[] args){
+        double[] list = {1,2,3,4,5,6};
+
+        double sum= 0.0;
+
+        for (int i =0;  i< list.length; i++){
+            sum += list[i];
+        }
+
+        double avarage =  sum / list.length;
+        System.out.println(avarage);
+
+
+    }
+}
