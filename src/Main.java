0540d654ce05a8330e7a9c57dc4eaa83df9db39f
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2025-11-14 10:33:50 +0300

    cok boyutlu dizi A harfi yazma

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,67 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-    int s1, s2, select;
+        /*   Arrays Sınıfının Metotları
 
-        System.out.print("Lütfen bir sayı giriniz : ");
+        *    Arrays.toString()
+        *    Arrays.fill()
+        *    Arrays.sort()
+        *    Arrays.binarySearch()
+        *    Arrays.copyOf()  ve Arrays.copyOfRange()
+        *    Arrays.equals()
 
-        Scanner input = new Scanner(System.in);
-        s1 = input.nextInt();
 
-        System.out.print("Lütfen ikinci  sayıyı giriniz : ");
-        s2 = input.nextInt();
 
-        System.out.println("Lütfen yapmak istediğiniz işlemi seçin");
-        System.out.println("1. Toplama \n 2. Çıkarma \n 3. Çarpma \n 4.Bölme ");
+        int[] dizi = {3, 5, 79, 12, 25, -3, 66, 82, -49, 152};
+        System.out.println(Arrays.toString(dizi));
 
-        select = input.nextInt();
 
-        switch (select) {
-            case 1:
-            System.out.println(s1 + s2);
-            break;
-            case 2:
-                System.out.println(s1 - s2);
-                break;
-            case 3:
-                System.out.println(s1 * s2);
-                break;
-            case 4:
-                if (s2 != 0) {
-                    System.out.println(s1 / s2);
-                } else
-                    System.out.println("bölmede payda sıfır olmaz");
+        int[] liste = {15, 1, 99, 7, 7, -22, 11, 2, -49, 52};
 
-                break;
-            default:
-                System.out.println("d");
-        }
+        Arrays.fill(liste, 2);
+        System.out.println(Arrays.toString(liste));
 
+        int[] liste2 = {15, 1, 99, 7, 7, -22, 11, 2, -49, 52};
+
+        Arrays.fill(liste2, 3, 5, 7);
+        System.out.println(Arrays.toString(liste2));
+
+
+        int[] liste = {6, 1, 55, 21, 33, -321, -21, 2, -11, 27};
+        Arrays.sort(liste);
+        System.out.println(Arrays.toString(liste));
+
+
+
+
+
+        int[] liste = {6, 1, 55, 21, 33, -321, -21, 2, -11, 27};
+
+        int[] copyArray = Arrays.copyOf(liste, 3);
+        System.out.println(Arrays.toString(copyArray));
+
+        int[] copyOfRangeArray = Arrays.copyOfRange(liste, 0,5);
+        System.out.println(Arrays.toString(copyOfRangeArray));
+
+
+        int[] list1 = {1, 2, 3};
+        int[] list2 = {1, 2, 3};
+        int[] list3 = {1, 2, 10};
+
+        System.out.println(Arrays.equals(list1, list2)); // true
+        System.out.println(Arrays.equals(list2, list3)); // false
+int[] liste = {6, 1, 55, 21, 33, -321, -21, 2, -11, 27};
+
+        Arrays.sort(liste);
+        System.out.println(liste[8]);
+        System.out.println(Arrays.toString(liste));
+
+        int index = Arrays.binarySearch(liste, 33);
+        System.out.println("33'ün indeksi :" + index);
+        */
 
 
 
